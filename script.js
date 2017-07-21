c708b35fce3cdfece9ab873746658143457ca0f5
Stephen <stephen.passero@gmail.com>
2017-07-21 15:50:21 -0400

    Made AI not move after game had been won

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 /*
-This project isn't done yet.
+AI picks a random spot and moves there...
+I didn't really have time to make the AI block the player or stuff like that...
  */
 function clear() {
     setTimeout(function() {
-        for (var i = 0; i < 10; i++) {
+        for (let i = 0; i < 10; i++) {
             $('#box' + i).removeClass('x');
             $('#box' + i).removeClass('o');
             $('#box' + i).empty();
             $('#box' + i).css('background-color', 'whitesmoke');
+            gameOver = false;
         }
     }, 1000);
+
     newGame();
 }
 
@@ -17,12 +20,14 @@ function checkIfGameOver(xOrO) {
     if ($('#box1').hasClass(xOrO) && $('#box2').hasClass(xOrO) && $('#box3').hasClass(xOrO)) {
         $('#box1').css('background-color', 'green');
         $('#box2').css('background-color', 'green');
+        gameOver = true;
         $('#box3').css('background-color', 'green');
         setTimeout(function() {
             alert(xOrO.toUpperCase() + ' Wins!');
         }, 100);
         clear();
     } else if ($('#box4').hasClass(xOrO) && $('#box5').hasClass(xOrO) && $('#box6').hasClass(xOrO)) {
+        gameOver = true;
         $('#box4').css('background-color', 'green');
         $('#box5').css('background-color', 'green');
         $('#box6').css('background-color', 'green');
@@ -31,6 +36,7 @@ function checkIfGameOver(xOrO) {
         }, 100);
         clear();
     } else if ($('#box7').hasClass(xOrO) && $('#box8').hasClass(xOrO) && $('#box9').hasClass(xOrO)) {
+        gameOver = true;
         $('#box7').css('background-color', 'green');
         $('#box8').css('background-color', 'green');
         $('#box9').css('background-color', 'green');
@@ -39,6 +45,7 @@ function checkIfGameOver(xOrO) {
         }, 100);
         clear();
     } else if ($('#box1').hasClass(xOrO) && $('#box4').hasClass(xOrO) && $('#box7').hasClass(xOrO)) {
+        gameOver = true;
         $('#box1').css('background-color', 'green');
         $('#box4').css('background-color', 'green');
         $('#box7').css('background-color', 'green');
@@ -47,6 +54,7 @@ function checkIfGameOver(xOrO) {
         }, 100);
         clear();
     } else if ($('#box2').hasClass(xOrO) && $('#box5').hasClass(xOrO) && $('#box8').hasClass(xOrO)) {
+        gameOver = true;
         $('#box2').css('background-color', 'green');
         $('#box5').css('background-color', 'green');
         $('#box8').css('background-color', 'green');
@@ -55,6 +63,7 @@ function checkIfGameOver(xOrO) {
         }, 100);
         clear();
     } else if ($('#box3').hasClass(xOrO) && $('#box6').hasClass(xOrO) && $('#box9').hasClass(xOrO)) {
+        gameOver = true;
         $('#box3').css('background-color', 'green');
         $('#box6').css('background-color', 'green');
         $('#box9').css('background-color', 'green');
@@ -63,6 +72,7 @@ function checkIfGameOver(xOrO) {
         }, 100);
         clear();
     } else if ($('#box1').hasClass(xOrO) && $('#box5').hasClass(xOrO) && $('#box9').hasClass(xOrO)) {
+        gameOver = true;
         $('#box1').css('background-color', 'green');
         $('#box5').css('background-color', 'green');
         $('#box9').css('background-color', 'green');
@@ -71,6 +81,7 @@ function checkIfGameOver(xOrO) {
         }, 100);
         clear();
     } else if ($('#box3').hasClass(xOrO) && $('#box5').hasClass(xOrO) && $('#box7').hasClass(xOrO)) {
+        gameOver = true;
         $('#box3').css('background-color', 'green');
         $('#box5').css('background-color', 'green');
         $('#box7').css('background-color', 'green');
@@ -80,32 +91,39 @@ function checkIfGameOver(xOrO) {
         clear();
     }
 }
-
+let gameOver = false;
 function newGame() {
     $(document).ready(function() {
-        var clicks = 0;
-        var oHasBeenPlaced = true;
+        let oHasBeenPlaced = true;
         $('div').click(function(event) {
             const divClickedId = $(this).attr("id");
             if ($('#' + divClickedId).is(':empty')) {
                 $(event.target).html('X');
                 $('#' + divClickedId).addClass('x');
                 checkIfGameOver('x');
-                clicks++;
                 oHasBeenPlaced = false;
-                if (clicks >= 8) {
-                    oHasBeenPlaced = true;
-                }
             }
+            let numbersTried = [];
             while (oHasBeenPlaced === false) {
-                var randomNumber = Math.floor((Math.random() * 9) + 1);
-                var divId = '#box' + randomNumber;
+                let randomNumber = Math.floor((Math.random() * 9) + 1);
+                numbersTried.push(randomNumber);
+                let divId = '#box' + randomNumber;
                 if ($(divId).is(':empty')) {
-                    $(divId).html('O');
-                    $(divId).addClass('o');
-                    clicks++;
-                    oHasBeenPlaced = true;
-                    checkIfGameOver('o');
+                    numbersTried = [];
+                    if(gameOver === false){
+                        $(divId).html('O');
+                        $(divId).addClass('o');
+                        oHasBeenPlaced = true;
+                        checkIfGameOver('o');
+                    }
+                    return;
+                }else if($.inArray( 1, numbersTried ) > -1 && $.inArray( 2, numbersTried ) > -1 &&
+                    $.inArray( 3, numbersTried ) > -1 && $.inArray( 4, numbersTried ) > -1 &&
+                    $.inArray( 5, numbersTried ) > -1 && $.inArray( 6, numbersTried ) > -1 &&
+                    $.inArray( 7, numbersTried ) > -1 && $.inArray( 8, numbersTried ) > -1 &&
+                    $.inArray( 9, numbersTried ) > -1){
+                    clear();
+                    return;
                 }
             }
         });
